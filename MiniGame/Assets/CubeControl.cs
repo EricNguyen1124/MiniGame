b14fc114e6ba77eeaf26cc44d21344e13bf37558
SharkBearGator-PC <47893402+SharkBearGator@users.noreply.github.com>
2019-10-10 01:46:04 -0500

    spawner

---
+++ MiniGame/Assets/CubeControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+
+    Transform tR;
+    Rigidbody rB;
+
+    float fallSpeed = 0.02f;
+    bool contact = false;
+
+    void Start()
+    {
+        tR = GetComponent<Transform>();
+        rB = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        if (contact == false)
+        {
+
+            Vector3 finalVector = new Vector3(horiz, 0f, vert);
+            finalVector.Normalize();
+            finalVector = finalVector * 0.05f;
+
+            tR.position = tR.position + finalVector;
+
+            tR.position = tR.position + new Vector3(0, -fallSpeed, 0);
+        }
+        else
+        {
+            rB.useGravity = true;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        contact = true;
+    }
+}
---
+++ MiniGame/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject myPrefab;
+    Vector3 randomSize;
+    float spawnOffset;
+
+    void Start()
+    {
+        StartCoroutine(Delayer());
+        spawnOffset = 5f;
+    }
+
+    IEnumerator Delayer()
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            yield return new WaitForSeconds(2);
+
+            randomSize.Set(Random.Range(0.5f, 3f), Random.Range(0.5f, 3f), Random.Range(0.5f, 3f));
+            myPrefab.transform.localScale = randomSize;
+            Instantiate(myPrefab, new Vector3(0, spawnOffset, 0), Quaternion.identity);
+
+            spawnOffset = spawnOffset + randomSize.y + 1f;
+        }
+        Debug.Log("DONE");
+    }
+}
